5c48aa12b25a8ec41a3ccca7830164bf918c148e
summerschneider <sschneider22@my.wctc.edu>
2022-12-07 16:41:29 -0600

    Added code for final project-adding a user and adding a movie rating

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MovieLibraryEntities.Context;
 using MovieLibraryEntities.Models;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MovieLibraryEntities
 {
@@ -13,11 +17,14 @@ namespace MovieLibraryEntities
     {
         static void Main(string[] args)
         {
+            var factory = LoggerFactory.Create(b => b.AddConsole());
+            var logger = factory.CreateLogger<Program>();
+
             string userInput;
             do
             {
                 Console.WriteLine("Enter your selection:");
-                Console.WriteLine("1) Search movie\n2) Add movie\n3) Update movie\n4) Delete movie\n5) Exit");
+                Console.WriteLine("1) Display movie\n2) Add movie\n3) Update movie\n4) Delete movie\n5) Add user\n6) Add movie rating\n7) Exit");
                 userInput = Convert.ToString(Console.ReadLine());
 
                 switch (userInput)
@@ -52,6 +59,7 @@ namespace MovieLibraryEntities
                                                         }
                                                         Console.WriteLine();
                                                     }
+                                                    logger.LogInformation("Displaying movies.");
                                                 }
                                                 else
                                                 {
@@ -73,6 +81,7 @@ namespace MovieLibraryEntities
                                             {
                                                 Console.WriteLine($"Movie {movie.Id}: {movie.Title}, Release Date: {movie.ReleaseDate}");
                                             }
+                                            logger.LogInformation("Displaying movies.");
                                             Console.WriteLine();
                                             break;
                                         }
@@ -113,6 +122,7 @@ namespace MovieLibraryEntities
                                         ReleaseDate = releaseDate
                                     };
                                     db.Movies.Add(movie);
+                                    logger.LogInformation("Adding movie.");
                                     db.SaveChanges();
 
                                     var newMovie = db.Movies.Where(x => x.Title == titleUserInput).FirstOrDefault();
@@ -161,6 +171,7 @@ namespace MovieLibraryEntities
                                     updateMovie.ReleaseDate = releaseDateUpdate;
 
                                     db.Movies.Update(updateMovie);
+                                    logger.LogInformation("Updating movie.");
                                     db.SaveChanges();
 
                                     Console.WriteLine("\nHere is the updated movie information:");
@@ -199,6 +210,7 @@ namespace MovieLibraryEntities
                                                 case "1":
                                                     {
                                                         db.Movies.Remove(deleteMovie);
+                                                        logger.LogInformation("Deleting movie.");
                                                         db.SaveChanges();
 
                                                         Console.WriteLine($"Movie {deleteMovie.Id}: {deleteMovie.Title}, Release Date: {deleteMovie.ReleaseDate}");
@@ -231,7 +243,217 @@ namespace MovieLibraryEntities
                             }
                             break;
                         }
-                    case"5":
+                    case "5": //add user w/ occupation
+                        {
+                            using (var db = new MovieContext())
+                            {
+                                Console.WriteLine("Input the age of the user you would like to add:");
+                                var ageUserInput = Console.ReadLine();
+                                bool isNullAge = string.IsNullOrEmpty(Convert.ToString(ageUserInput));
+                                int age;
+                                bool isIntAge = int.TryParse(ageUserInput, out age);
+
+                                if (isNullAge == false && isIntAge == true)
+                                {
+                                    Console.WriteLine("Input the gender of the user you would like to add:");
+                                    var genderUserInput = Console.ReadLine();
+                                    bool isNullGender = string.IsNullOrEmpty(genderUserInput);
+
+                                    if (isNullGender == false)
+                                    {
+                                        Console.WriteLine("Input the zip code of the user you would like to add:");
+                                        var zipCodeUserInput = Console.ReadLine();
+                                        bool isNullZipCode = string.IsNullOrEmpty(zipCodeUserInput);
+
+                                        if (isNullZipCode == false)
+                                        {
+                                            Console.WriteLine("\nHere is the list of occupations:\n---------------------------");
+                                            foreach (var occ in db.Occupations)
+                                            {
+                                                Console.WriteLine($"Occupation ID: {occ.Id}, {occ.Name}");
+                                            }
+                                            Console.WriteLine("\nInput the occupation ID of the user you would like to add:");
+                                            var occupationUserInput = Console.ReadLine();
+
+                                            bool isNullOccupation = string.IsNullOrEmpty(Convert.ToString(occupationUserInput));
+                                            int occupationInt;
+                                            bool isIntOccupation = int.TryParse(occupationUserInput, out occupationInt);
+                                            if (isNullOccupation == false && isIntOccupation == true)
+                                            {
+                                                var occupation = db.Occupations.Where(x => x.Id == occupationInt).FirstOrDefault();
+
+                                                var user = new User()
+                                                {
+                                                    Age = age,
+                                                    Gender = genderUserInput,
+                                                    ZipCode = zipCodeUserInput,
+                                                };
+                                                user.Occupation = occupation;
+
+                                                db.Users.Add(user);
+                                                logger.LogInformation("Adding user.");
+                                                db.SaveChanges();
+
+                                                var newUser = db.Users.Where(x => x.Age == age && x.Gender == genderUserInput && x.ZipCode == zipCodeUserInput).FirstOrDefault();
+                                                Console.WriteLine($"User {newUser.Id}, Age: {newUser.Age}, Gender: {newUser.Gender}, ZipCode: {newUser.ZipCode}, Occupation: {newUser.Occupation.Name}");
+                                                Console.WriteLine("\tThis user has been added\n");
+                                            }
+                                            else if (isNullOccupation == true)
+                                            {
+                                                Console.WriteLine("User occupation ID cannot be null\n");
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("Incorrect input of user occupation ID\n");
+                                                break;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("User zip code cannot be null\n");
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("User gender cannot be null\n");
+                                        break;
+                                    }
+                                }
+                                else if (isNullAge == true)
+                                {
+                                    Console.WriteLine("User age cannot be null\n");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Incorrect input of user age\n");
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    case "6": //add movie rating
+                        {
+                            using (var db = new MovieContext())
+                            {
+                                string yesOrNo;
+                                do
+                                {
+                                    Console.WriteLine("Input the title of the movie you would like to rate:");
+                                    var movieUserInput1 = Console.ReadLine();
+                                    bool isNullTitle = string.IsNullOrEmpty(movieUserInput1);
+
+                                    if (isNullTitle == false)
+                                    {
+                                        var rateMovie = db.Movies.FirstOrDefault(x => x.Title.ToUpper().Contains(movieUserInput1.ToUpper()));
+                                        if (rateMovie is not null)
+                                        {
+                                            Console.WriteLine($"Movie {rateMovie.Id}: {rateMovie.Title}, Release Date: {rateMovie.ReleaseDate}");
+
+                                            Console.WriteLine("\nIs this the movie you would like to rate? (1-Yes, 2-No)");
+                                            yesOrNo = Convert.ToString(Console.ReadLine());
+                                            switch (yesOrNo)
+                                            {
+                                                case "1":
+                                                    {
+                                                        Console.WriteLine("Input the ID of the user rating the movie:");
+                                                        var userInput1 = Console.ReadLine();
+
+                                                        bool isNullUser = string.IsNullOrEmpty(userInput1);
+                                                        int userInt;
+                                                        bool isIntUser = int.TryParse(userInput1, out userInt);
+
+                                                        if (isNullUser == false && isIntUser == true)
+                                                        {
+                                                            var rateUser = db.Users.Where(x => x.Id == Convert.ToInt32(userInput1)).FirstOrDefault();
+
+                                                            Console.WriteLine("Input the rating of the movie (1-5):");
+                                                            var movieRatingInput = Console.ReadLine();
+
+                                                            bool isNullRating = string.IsNullOrEmpty(movieRatingInput);
+                                                            int ratingInt;
+                                                            bool isIntRating = int.TryParse(movieRatingInput, out ratingInt);
+
+                                                            if (isNullRating == false && isIntRating == true)
+                                                            {
+                                                                if (movieRatingInput == "1" || movieRatingInput == "2" || movieRatingInput == "3" || movieRatingInput == "4" || movieRatingInput == "5")
+                                                                {
+                                                                    var userMovie = new UserMovie()
+                                                                    {
+                                                                        Rating = ratingInt,
+                                                                        RatedAt = DateTime.Now
+                                                                    };
+
+                                                                    userMovie.User = rateUser;
+                                                                    userMovie.Movie = rateMovie;
+
+                                                                    db.UserMovies.Add(userMovie);
+                                                                    logger.LogInformation("Adding movie rating.");
+                                                                    db.SaveChanges();
+
+                                                                    var newUserMovie = db.UserMovies.Where(x => x.Rating == ratingInt && x.User == rateUser && x.Movie == rateMovie).FirstOrDefault();
+                                                                    Console.WriteLine($"Movie {newUserMovie.Movie.Id}: {newUserMovie.Movie.Title}, Rated by User {newUserMovie.User.Id}:");
+                                                                    Console.WriteLine($"User Movie {newUserMovie.Id}, Rating: {newUserMovie.Rating}, Rated at: {newUserMovie.RatedAt}");
+                                                                    Console.WriteLine("\tThis movie rating has been added\n");
+                                                                    break;
+                                                                }
+                                                                else
+                                                                {
+                                                                    Console.WriteLine("Incorrect input of movie rating, it must be 1-5\n");
+                                                                    break;
+                                                                }
+                                                            }
+                                                            else if (isNullRating == true)
+                                                            {
+                                                                Console.WriteLine("Movie rating cannot be null\n");
+                                                                break;
+                                                            }
+                                                            else
+                                                            {
+                                                                Console.WriteLine("Incorrect input of movie rating\n");
+                                                                break;
+                                                            }
+                                                        }
+                                                        else if (isNullUser == true)
+                                                        {
+                                                            Console.WriteLine("User ID cannot be null\n");
+                                                            break;
+                                                        }
+                                                        else
+                                                        {
+                                                            Console.WriteLine("Incorrect input of user ID\n");
+                                                            break;
+                                                        }
+                                                    }
+                                                case "2":
+                                                    {
+                                                        break;
+                                                    }
+                                                default:
+                                                    {
+                                                        Console.WriteLine("Incorrect input\n");
+                                                        break;
+                                                    }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Inputted movie title does not exist\n");
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Movie title cannot be null\n");
+                                        break;
+                                    }
+                                } while (yesOrNo == "2");
+                            }
+                            break;
+                        }
+                    case "7": //exit
                         {
                             break;
                         }
@@ -241,8 +463,7 @@ namespace MovieLibraryEntities
                             break;
                         }
                 }
-
-            } while (userInput != "5");
+            } while (userInput != "7");
         }
     }
 }
